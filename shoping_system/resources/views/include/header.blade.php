1d9d793df1e5610d28edbc307413f9930dfb7ea3
Himashi99Kodithuwakku <kodi.99hima@gmail.com>
2023-09-13 12:23:56 +0530

    created home page

---
+++ shoping_system/resources/views/include/header.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ABC</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mobile Category
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">iPhone</a></li>
+            <li><a class="dropdown-item" href="#">Samsung</a></li>
+            <li><a class="dropdown-item" href="#">Sony</a></li>
+            <li><a class="dropdown-item" href="#">Huawei</a></li>
+            <li><a class="dropdown-item" href="#">Oppo</a></li>
+            <li><a class="dropdown-item" href="#">Nokia</a></li>
+            <li><a class="dropdown-item" href="#">Realme</a></li>
+            <li><a class="dropdown-item" href="#">Vivo</a></li>
+          </ul>
+        </li>
+      </ul>
+
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+      <ul>
+      
+      <button class="btn btn-outline-success" type="submit">Login</button>
+      <button class="btn btn-outline-success" type="submit">Register</button>
+      </ul>
+    </div>
+  </div>
+</nav>
--- shoping_system/routes/web.php
+++ shoping_system/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
+});
+
+Route::get('/', function () {
+    return view('include.products');
 });
